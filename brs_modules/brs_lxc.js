638f8b6a897515f84acd1abf1e65778195181f21
Brs Grzlski <Boris@slav.rocks>
2017-04-11 14:09:43 +0200

    brs_lxc -> create & start

--- brs_modules/brs_lxc.js
+++ brs_modules/brs_lxc.js
@@ -6,11 +6,42 @@ function lxc(args) {
 	this.ip = args.ip;
 	this.subnet = config.lxc_subnet;
 	this.gateway = config.lxc_gateway;
+	this.dns = config.lxc_dns;
 	this.share = config.lxc_share;
 }
 
 lxc.prototype.start = function start() {
   console.log(this);
 };
+
+lxc.prototype.create = function() {
+	args = {
+		cmd:'/usr/bin/lxc-create -n '+this.name+' -t romavpn -- --ip '+this.ip+' --subnet '+this.subnet+' --gateway '+this.gateway+' --dns '+this.dns+' --share '+this.share,
+		cf_out: function(data){
+			console.log(data);
+		},
+		cf_err: function(data){
+			console.log(data);
+		},
+		cf_finish: function(){
+			console.log('Lxc container created');
+		}
+	}
+}
+
+lxc.prototype.start = function() {
+	args = {
+		cmd : '/usr/bin/lxc-start -n '+this.name,
+		cf_out: function(data){
+			console.log(data);
+		},
+		cf_err: function(data){
+			console.log(data);
+		},
+		cf_finish: function (data) {
+			console.log('Lxc container started');
+		}
+	}
+}
 
 module.exports = lxc;
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	'logfile' : '/var/log/romavpn.log',
 	'lxc_subnet' : '255.255.255.0',
-	'lxc_gateway' : '10.0.0.254',
+	'lxc_gateway' : '10.1.0.254',
+	'lxc_dns' : '10.0.0.253'
 	'lxc_share' : '/root/romavpn/openvpn_files',
 }
